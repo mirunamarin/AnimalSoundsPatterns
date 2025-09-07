b035de67c1204980e66d63bb8a88ebd0b24cfaad
Miruna Marin <marinlarisa14@gmail.com>
2025-09-07 19:25:22 +0300

    Add unit tests (Strategy & Factory Method)

---
+++ AnimalSoundsPatterns.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnimalSoundsPatterns.Animals;
+using AnimalSoundsPatterns.Factory;
+
+namespace AnimalSoundsPatterns.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Strategy_Dog_Sound_IsWoof()
+        {
+            IAnimal dog = new Dog();
+            Assert.AreEqual("Dog", dog.Type);
+            Assert.AreEqual("woof", dog.Sound);
+        }
+
+        [TestMethod]
+        public void FactoryMethod_CatCreator_CreatesCat()
+        {
+            var creator = new CatCreator();
+            IAnimal animal = creator.Create();
+
+            Assert.IsInstanceOfType(animal, typeof(Cat));
+            Assert.AreEqual("Cat", animal.Type);
+            Assert.AreEqual("meow", animal.Sound);
+        }
+    }
+}
